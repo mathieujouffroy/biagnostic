01ac8b7b1a644b200460a1a891deee858138e56e
mathieujouffroy <mathieu.jouffroy@gmail.com>
2022-11-23 15:25:29 +0100

    refactoring dataset class

--- train_framework/dataloader.py
+++ train_framework/dataloader.py
@@ -126,14 +126,14 @@ class BratsDatasetGenerator:
         return image, label
 
 
-    def load_one_example_nl(self, idx):
+    def load_example_nl(self, idx):
         niimg = nl.image.load_img(self.filenames[idx][0])
         nimask = nl.image.load_img(self.filenames[idx][1])
         return niimg, nimask
 
 
     def plot_nl(self, idx):
-        niimg, nimask = self.load_one_example_nl(idx)
+        niimg, nimask = self.load_example_nl(idx)
         niimg = niimg.slicer[:, :, :, 0]
         fig, axes = plt.subplots(nrows=4, figsize=(8, 10))
         nlplt.plot_img(niimg, axes=axes[0])
@@ -144,16 +144,10 @@ class BratsDatasetGenerator:
 
 
     def colorize_labels_image_flair(self, idx):
-
-        # load the image and label file, get the image content and return a numpy array for each
         image, label = self.load_example(idx)
-
-        # one hot encode labels
         label = to_categorical(label, num_classes=4).astype(np.uint8)
-
         # Normalize Flair channel
         image = cv2.normalize(image[:, :, :, 0], None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F).astype(np.uint8)
-
         un, cn = np.unique(image, return_counts=True)
         #print(np.asarray((un, cn)).T)
 
@@ -181,17 +175,17 @@ class BratsDatasetGenerator:
         fig, ax = plt.subplots(3, 6, figsize=[16, 9])
 
         # coronal plane
-        # transpose : batch, h, w, depth, channel -> height, depth, width, channel, batch
+        # batch, h, w, depth, channel -> height, depth, width, channel, batch
         coronal = np.transpose(data_all, [1, 3, 2, 4, 0])
         coronal = np.rot90(coronal, k=1)
 
         # transversal plane
-        # transpose : batch, h, w, depth, channel -> width, height, depth, channel, batch
+        # batch, h, w, depth, channel -> width, height, depth, channel, batch
         transversal = np.transpose(data_all, [2, 1, 3, 4, 0])
         transversal = np.rot90(transversal, k=2)
 
         # sagittal plane
-        # transpose : batch, h, w, depth, channel -> width, depth, height, channel, batch
+        # batch, h, w, depth, channel -> width, depth, height, channel, batch
         sagittal = np.transpose(data_all, [2, 3, 1, 4, 0])
         sagittal = np.rot90(sagittal, k=1)
 
@@ -273,25 +267,23 @@ class BratsDatasetGenerator:
         tries = 0
 
         while tries < max_tries:
-            # randomly sample sub-volume by sampling the corner voxel (make sure to leave enough room for the output dimensions)
-            start_x = (orig_x - output_x + 1)//2
-            start_y = (orig_y - output_y + 1)//2
-            
+            # randomly sample sub-volume by sampling the corner voxel
+            # (make sure to leave enough room for the output dimensions)
+            min_x = min(np.where(label != 0)[0])
+            max_x = max(np.where(label != 0)[0])
+            min_y = min(np.where(label != 0)[1])
+            max_y = max(np.where(label != 0)[1])
             min_z = min(np.where(label != 0)[2])
             max_z = max(np.where(label != 0)[2])
-            
-            min_label_x = min(np.where(label != 0)[0])
-            max_label_x = max(np.where(label != 0)[0])
-            
-            min_label_y = min(np.where(label != 0)[1])
-            max_label_y = max(np.where(label != 0)[1])
-
             len_z = max_z - min_z
-            
-            if start_x > min_label_x - 8:
-                start_x = min_label_x - 8
-            if start_y > min_label_y - 8:
-                start_y = min_label_y - 8
+
+            start_x = (orig_x - output_x + 1)//2
+            start_y = (orig_y - output_y + 1)//2
+
+            if start_x > min_x - 8:
+                start_x = min_x - 8
+            if start_y > min_y - 8:
+                start_y = min_y - 8
 
             if (max_z - min_z <= 60):
                 if (max_z - min_z <= 32):
@@ -299,16 +291,16 @@ class BratsDatasetGenerator:
                 else:
                     start_z = np.random.randint(min_z, max_z-output_z)
 
-                #print(f"Start X: {start_x}, end_X: {start_x+output_x}, min_label_x: {min_label_x}, max_x: {max_label_x}")
-                #print(f"Start Y: {start_y}, end_Y: {start_y+output_y}, min_label_y: {min_label_y}, max_y: {max_label_y}")
-                #print(f"Start Z: {start_z}, end_Z: {start_z+output_z}, min_label_z: {min_z}, max_z: {max_z}, len_Z: {max_z-min_z}")
+                #print(f"Start X: {start_x}, end_X: {start_x+output_x}, min_x: {min_x}, max_x: {max_x}")
+                #print(f"Start Y: {start_y}, end_Y: {start_y+output_y}, min_y: {min_y}, max_y: {max_y}")
+                #print(f"Start Z: {start_z}, end_Z: {start_z+output_z}, min_z: {min_z}, max_z: {max_z}, len_Z: {max_z-min_z}")
                 y = label[start_x: start_x + output_x,
                           start_y: start_y + output_y,
                           start_z: start_z + output_z]
-                
+
                 # One-hot encode the categories -> (output_x, output_y, output_z, n_classes)
                 y = to_categorical(y, num_classes = self.n_classes)
-                
+
                 X = np.copy(image[start_x: start_x + output_x,
                                       start_y: start_y + output_y,
                                       start_z: start_z + output_z, :])
@@ -329,8 +321,7 @@ class BratsDatasetGenerator:
             elif len_z > 75 and len_z <= 85:
                 background_threshold=0.969
 
-
-            if (start_x + output_x > max_label_x) and (start_y+output_y > max_label_y):
+            if (start_x + output_x > max_x) and (start_y+output_y > max_y):
 
                 y = label[start_x: start_x + output_x,
                           start_y: start_y + output_y,
@@ -344,11 +335,10 @@ class BratsDatasetGenerator:
 
                 tries += 1
                 print(f"background_ratio: {bgrd_ratio}")
-                #print(f"Start X: {start_x}, end_X: {start_x+output_x}, min_label_x: {min_label_x}, max_x: {max_label_x}")
-                #print(f"Start Y: {start_y}, end_Y: {start_y+output_y}, min_label_y: {min_label_y}, max_y: {max_label_y}")
-                #print(f"Start Z: {start_z}, end_Z: {start_z+output_z}, min_label_z: {min_z}, max_z: {max_z}, len_Z: {max_z-min_z}")
                 if bgrd_ratio < background_threshold:
-
+                    #print(f"Start X: {start_x}, end_X: {start_x+output_x}, min_x: {min_x}, max_x: {max_x}")
+                    #print(f"Start Y: {start_y}, end_Y: {start_y+output_y}, min_y: {min_y}, max_y: {max_y}")
+                    #print(f"Start Z: {start_z}, end_Z: {start_z+output_z}, min_z: {min_z}, max_z: {max_z}, len_Z: {max_z-min_z}")
                     X = np.copy(image[start_x: start_x + output_x,
                                       start_y: start_y + output_y,
                                       start_z: start_z + output_z, :])
@@ -359,11 +349,11 @@ class BratsDatasetGenerator:
                     # take a subset of y that excludes the background class in the 'n_classes' dimension
                     y = y[1:, :, :, :]
                     X = self.standardize(X)
-                    print("\n\n --------- BIIIM -------- \n\n")
+                    print("\n -------------------- \n")
                     return X, y, start_x, start_y, start_z
 
         print("No valid sub volume")
-        print(f"Start Z: {start_z}, end_Z: {start_z+output_z}, min_label_z: {min_z}, max_z: {max_z}, len_Z: {max_z-min_z}")
+        print(f"Start Z: {start_z}, end_Z: {start_z+output_z}, min_z: {min_z}, max_z: {max_z}, len_Z: {max_z-min_z}")
 
 
     def create_volume_sets(self):
@@ -396,9 +386,7 @@ class BratsDatasetGenerator:
             json.dump(ds_dict, f, indent=4)
 
 
-
     def get_sub_volume(self, idx, config, set_type):
-        #f"{i}_BRATS"
         files = config[set_type]['files']
         cur_file = files[str(idx)][:-3]
         start_x = int(cur_file.split('_')[-3])
@@ -427,39 +415,6 @@ class BratsDatasetGenerator:
         return img, mask
 
 
-    def get_dataset(self):
-        ds = tf.data.Dataset.range(self.numFiles).shuffle(self.numFiles, self.seed) # Shuffle the dataset
-
-        ds_train = ds.take(self.len_train)
-        ds_val_test = ds.skip(self.len_train)
-        ds_val = ds_val_test.take(int(self.val_test_len * self.val_test_split))
-        ds_test = ds_val_test.skip(int(self.val_test_len * self.val_test_split))
-
-
-        ds_train = ds_train.map(lambda x: tf.py_function(self.generate_sub_volume, [x], [tf.float32, tf.float32]), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        ds_val = ds_val.map(lambda x: tf.py_function(self.generate_sub_volume, [x], [tf.float32, tf.float32]), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        ds_test = ds_test.map(lambda x: tf.py_function(self.generate_sub_volume, [x], [tf.float32, tf.float32]), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-        ds_train = ds_train.batch(1)
-        ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
-
-        ds_val = ds_val.batch(1)
-        ds_val = ds_val.prefetch(tf.data.experimental.AUTOTUNE)
-
-        ds_test = ds_test.batch(1)
-        ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
-
-        return ds_train, ds_val, ds_test
-
-
-    def get_training_sets(self):
-        return self.ds_train, self.ds_val
-
-
-    def get_test_set(self):
-        return self.ds_test
-
-
     def visualize_patch(self, image, mask, m_chan=0):
         # Flair channel
         image = image[0, :, :, :]
@@ -478,6 +433,29 @@ class BratsDatasetGenerator:
             fig.subplots_adjust(wspace=0, hspace=0)
             fig.suptitle(f'Inspection on depth {i}')
 
+
+    def get_dataset(self):
+        ds = tf.data.Dataset.range(self.numFiles).shuffle(self.numFiles, self.seed) # Shuffle the dataset
+        ds_train = ds.take(self.len_train)
+        ds_val_test = ds.skip(self.len_train)
+        ds_val = ds_val_test.take(int(self.val_test_len * self.val_test_split))
+        ds_test = ds_val_test.skip(int(self.val_test_len * self.val_test_split))
+        ds_train = ds_train.map(lambda x: tf.py_function(self.generate_sub_volume, [x], [tf.float32, tf.float32]), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_val = ds_val.map(lambda x: tf.py_function(self.generate_sub_volume, [x], [tf.float32, tf.float32]), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_test = ds_test.map(lambda x: tf.py_function(self.generate_sub_volume, [x], [tf.float32, tf.float32]), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_train = ds_train.batch(1)
+        ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+        ds_val = ds_val.batch(1)
+        ds_val = ds_val.prefetch(tf.data.experimental.AUTOTUNE)
+        ds_test = ds_test.batch(1)
+        ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+        return ds_train, ds_val, ds_test
+
+    def get_training_sets(self):
+        return self.ds_train, self.ds_val
+
+    def get_test_set(self):
+        return self.ds_test
 
 
 
@@ -547,7 +525,7 @@ class VolumeDataGenerator(tf.keras.utils.Sequence):
 
         return X, y
 
-    
+
     def on_epoch_end(self):
         'Updates indexes after each epoch'
         self.indexes = np.arange(len(self.list_IDs))
